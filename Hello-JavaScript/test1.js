725ffc9c06ae7d6cc2a0ae6862968d33bf87b844
tottisilva <73293985+tottisilva@users.noreply.github.com>
2020-11-12 13:38:51 +0000

    JavaScript-loops

---
+++ Hello-JavaScript/test1.js
@@ -0,0 +1,13 @@
+//var age = 29;
+//var message = "Hi, my name is " + name + " and i have " + age + " years old";
+// alert(message);
+
+//console.log(message);
+//var total = 10;
+//for (var x = 0; x < total; x++){
+//  console.log(x);
+//}
+var students = ["José", "Joana", "Maria", "Joel", "Fernando"];
+for (var index = 0; index < students.length; index++){
+  console.log(students[index]);
+}
